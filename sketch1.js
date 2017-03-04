d495b8608ebd0aa16d8a1880e0bcd1cc333a9df6
AlexHeselton <alex.heselton@gmail.com>
2017-03-04 13:29:34 -0800

    Sketch 2 - bubble pulsates if mouse is pressed

--- sketch1.js
+++ sketch1.js
@@ -1,4 +1,8 @@
-
+var startRadius = 15;
+var maxRadius = 18;
+var radius = 15;
+var growthSpeed = 0.40;
+var grow = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,8 +13,26 @@ function draw() {
   background(201);
   translate(mouseX, mouseY);
   scale(mouseX / 50.0);
-  ellipse(0, 0, 15, 15);
+  ellipse(0, 0, radius, radius);
 
+  if(mouseIsPressed) {
+    if (radius <= startRadius) {
+        radius += growthSpeed;
+        grow = true;
+      }
+      else if ((radius > startRadius) && (radius < maxRadius)) {
+        if (grow == true) {
+          radius += growthSpeed;
+        }
+        else {
+          radius -= growthSpeed;
+        }
+      }
+      else {
+        radius -= growthSpeed;
+        grow = false;
+      }
+  }
 
 }
   
