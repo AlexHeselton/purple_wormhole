54f5d5ff75280be7230963084a3c7ecedaac8a70
AlexHeselton <alex.heselton@gmail.com>
2017-03-04 12:58:56 -0800

    Sketch 1

--- sketch1.js
+++ sketch1.js
@@ -1,12 +1,15 @@
 
 
 function setup() {
-
-
+  createCanvas(windowWidth, windowHeight);
+  ellipseMode(RADIUS);
 }
 
 function draw() {
-
+  background(201);
+  translate(mouseX, mouseY);
+  scale(mouseX / 50.0);
+  ellipse(0, 0, 15, 15);
 
 
 }
